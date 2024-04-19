ae14ec7ac596f1fc430701692b7ccd9b48182887
†村神† <107534447+CA01971172@users.noreply.github.com>
2024-04-19 17:29:50 +0900

    Implement tabs

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,35 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './../assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [value, setValue] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Tabs
+                value={value}
+                onChange={(_event: React.SyntheticEvent, newValue: number) => setValue(newValue)}
+            >
+                <Tab label="Item One"/>
+                <Tab label="Item Two"/>
+                <Tab label="Item Three"/>
+            </Tabs>
+            <div hidden={value !== 0}>
+                Item One
+            </div>
+            <div hidden={value !== 1}>
+                Item One
+            </div>
+            <div hidden={value !== 2}>
+                Item One
+            </div>
+        </>
+    )
 }
 
 export default App
